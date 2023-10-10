35170304789a59382a17842bc085bf3805f05bf1
NXPBenjaminK <benjaminphilipp.ketterer@nxp.com>
2023-10-10 11:44:37 +0200

    removed wired change to mixer

--- src/lib/mixer_module/mixer_module.cpp
+++ src/lib/mixer_module/mixer_module.cpp
@@ -493,7 +493,7 @@ MixingOutput::limitAndUpdateOutputs(float outputs[MAX_ACTUATORS], bool has_updat
 	// We must calibrate the PWM and Oneshot ESCs to a consistent range of 1000-2000us (gets mapped to 125-250us for Oneshot)
 	// Doing so makes calibrations consistent among different configurations and hence PWM minimum and maximum have a consistent effect
 	// hence the defaults for these parameters also make most setups work out of the box
-	if (_armed.in_esc_calibration_mode) {
+	/*if (_armed.in_esc_calibration_mode) {
 		static constexpr uint16_t PWM_CALIBRATION_LOW = 1000;
 		static constexpr uint16_t PWM_CALIBRATION_HIGH = 2000;
 
@@ -506,7 +506,7 @@ MixingOutput::limitAndUpdateOutputs(float outputs[MAX_ACTUATORS], bool has_updat
 				_current_output_value[i] = PWM_CALIBRATION_HIGH;
 			}
 		}
-	}
+	}*/
 
 	/* now return the outputs to the driver */
 	if (_interface.updateOutputs(stop_motors, _current_output_value, _max_num_outputs, has_updates)) {
